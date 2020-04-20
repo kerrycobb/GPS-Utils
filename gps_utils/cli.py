c4fcbbfa9469a06ce1751a7026e0aaa80e1a12fe
Kerry Cobb <cobbkerry@gmail.com>
2020-04-20 14:30:37 -0500

    Changed subprocess.call to popen in auto_open_path function which makes xdg-open run in the background

--- gps_utils/cli.py
+++ gps_utils/cli.py
@@ -4,9 +4,9 @@ from .core import GPS
 
 # gps2csv
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
-@click.option("--device", default="GARMIN", 
+@click.option("--device", default="GARMIN",
         help="Name of device to get waypoints from. Default: GARMIN")
-@click.option("--output", default=None, type=click.File("wb"), 
+@click.option("--output", default=None, type=click.File("wb"),
         help="Path to output file. Default: <current workdir path>/<device name>.csv")
 def csv_cli(device, output):
     """Load waypoints from GPS into csv file"""
@@ -20,24 +20,24 @@ def csv_cli(device, output):
 
 #gps2map
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
-@click.option("--device", default="GARMIN", 
+@click.option("--device", default="GARMIN",
         help="Name of device to get data from. Default: GARMIN")
-@click.option("--output", default=None, type=click.File("wb"), 
+@click.option("--output", default=None, type=click.File("wb"),
         help="Path to output file. Default: <current workdir path>/<device name>.html")
 def map_cli(device, output):
     """Load data from GPS into Leaflet map"""
     gps = GPS(device=device)
-    gps.make_map(output=output, waypoints=True, current_track=True, 
+    gps.make_map(output=output, waypoints=True, current_track=True,
             archived_track=True, auto_open=True)
 
 # gps2kml
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
-@click.option("--device", default="GARMIN", 
+@click.option("--device", default="GARMIN",
         help="Name of device to get data from. Default: GARMIN")
-@click.option("--output", default=None, type=click.File("wb"), 
+@click.option("--output", default=None, type=click.File("wb"),
         help="Path to output file. Default: <current workdir path>/<device name>.kml")
 def kml_cli(device, output):
     """Load data from GPS into Google Earth"""
     gps = GPS(device=device)
-    gps.make_kml(output=output, waypoints=True, current_track=True, 
+    gps.make_kml(output=output, waypoints=True, current_track=True,
             archived_track=True, auto_open=True)
--- gps_utils/core.py
+++ gps_utils/core.py
@@ -5,6 +5,7 @@ import psutil
 from datetime import datetime, timezone
 from dateutil import tz
 import subprocess
+import collections
 
 import gpxpy
 import folium
@@ -15,21 +16,23 @@ import simplekml
 
 def auto_open_path(path):
     if sys.platform.startswith("darwin"):
-        subprocess.call(("open", path))
+        subprocess.Popen(["open", path], stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
     elif os.name == "nt":
         os.startfile(path)
     elif os.name == "posix":
-        subprocess.call(("xdg-open", path))
+        subprocess.Popen(["xdg-open", path], stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
     else:
-        print("Operating system not recognized. Unable to auto open KML")
- 
+        print("Operating system not recognized. Unable to auto open file")
+
 class GPS():
     def __init__(self, device="GARMIN"):
         partitions = psutil.disk_partitions()
         mount_paths = [p.mountpoint for p in partitions if device in p.mountpoint]
         if not mount_paths:
             sys.exit((
-                "Error!\n" 
+                "Error!\n"
                 "No device found with name: {}\n"
                 "Check that device is plugged in or that device name is correct\n"
                 "A custom device name can be provided using the option: --device").format(device))
@@ -39,7 +42,7 @@ class GPS():
         self.device_name = device
         self.waypoints = []
         self.current_track = []
-        self.archived_tracks = [] 
+        self.archived_tracks = []
 
     def get_waypoints(self):
         waypoints_dir_path = os.path.join(self.mount_path, "Garmin/GPX")
@@ -47,7 +50,7 @@ class GPS():
         for file in waypoint_gpx_files:
             gpx = gpxpy.parse(open(file))
             self.waypoints.extend(gpx.waypoints)
-    
+
     def get_current_tracks(self):
         current_file = os.path.join(self.mount_path, "Garmin/GPX/Current/Current.gpx")
         gpx = gpxpy.parse(open(current_file))
@@ -63,40 +66,57 @@ class GPS():
     def to_df(self):
         if not self.waypoints:
             self.get_waypoints()
-        data = [] 
+        data = []
         tf = TimezoneFinder()
         for wp in self.waypoints:
-            local_tz = tf.certain_timezone_at(lng=wp.longitude, lat=wp.latitude)
-            local_dt = wp.time.replace(tzinfo=timezone.utc).astimezone(tz=tz.gettz(local_tz))
-            data.append(collections.OrderedDict({
-                "name": wp.name,
-                "latitude": wp.latitude,
-                "longitude": wp.longitude,
-                "elev (m)": wp.elevation,
-                "year (local)": local_dt.year,
-                "month (local)": local_dt.month,
-                "day (local)": local_dt.day,
-                "time (local)": str(local_dt.time().strftime("%H:%M")),
-                "timezone (local)": local_tz,
-                "year (UTC)": wp.time.year,
-                "month (UTC)": wp.time.month,
-                "day (UTC)": wp.time.day,
-                "time (UTC)": str(wp.time.time().strftime("%H:%M")),
-            }))
+            d = collections.OrderedDict()
+            d["name"] = wp.name
+            d["latitude"] = wp.latitude
+            d["longitude"] = wp.longitude
+            d["elev (m)"] = wp.elevation
+            if wp.time:
+                local_tz = tf.certain_timezone_at(lng=wp.longitude, lat=wp.latitude)
+                local_dt = wp.time.replace(tzinfo=timezone.utc).astimezone(tz=tz.gettz(local_tz))
+                d["year (local)"] = local_dt.year
+                d["month (local)"] = local_dt.month
+                d["day (local)"] = local_dt.day
+                d["time (local)"] = str(local_dt.time().strftime("%H:%M"))
+                d["timezone (local)"] = local_tz
+                d["year (UTC)"] =  wp.time.year
+                d["month (UTC)"] = wp.time.month
+                d["day (UTC)"] = wp.time.day
+                d["time (UTC)"] = str(wp.time.time().strftime("%H:%M"))
+            else:
+                d["year (local)"] = None
+                d["month (local)"] = None
+                d["day (local)"] = None
+                d["time (local)"] = None
+                d["timezone (local)"] = None
+                d["year (UTC)"] = None
+                d["month (UTC)"] = None
+                d["day (UTC)"] = None
+                d["time (UTC)"] = None
+            data.append(d)
         df = pd.DataFrame(data)
         return df
 
-    def make_map(self, output=None, waypoints=True, current_track=True, 
+    def make_map(self, output=None, waypoints=True, current_track=True,
             archived_track=True, auto_open=True):
         m = folium.Map(tiles="Stamen Terrain")
+        folium.TileLayer("openstreetmap").add_to(m)
+        # TODO: Figure out how to add this layer, currently throws error
+        # folium.TileLayer("MapQuest Open Aerial").add_to(m)
+        folium.LayerControl().add_to(m)
         if waypoints:
             if not self.waypoints:
                 self.get_waypoints()
             for wp in self.waypoints:
-                folium.Marker(location=[wp.latitude, wp.longitude]).add_to(m)
+                m.add_child(folium.Marker(
+                    location=[wp.latitude, wp.longitude],
+                    popup=folium.Popup(self.__wp_data_str(wp), max_width=200)))
         if current_track:
             if not self.current_track:
-                self.get_current_tracks() 
+                self.get_current_tracks()
             for track in self.current_track:
                 self.__add_track_to_map(track, m)
         if archived_track:
@@ -107,13 +127,30 @@ class GPS():
         if output:
             map_path = output
         else:
-            map_path = os.path.join(os.getcwd(), 
+            map_path = os.path.join(os.getcwd(),
                     "{}.html".format(self.device_name))
         m.fit_bounds(m.get_bounds())
         m.save(map_path)
         if auto_open:
             auto_open_path(map_path)
-   
+
+    def __wp_data_str(self, wp, name=True, date=True):
+        popup = []
+        if wp.name:
+            if name:
+                popup.append(wp.name)
+        if wp.time:
+            if date:
+                popup.append("{year}-{month}-{day}".format(
+                    year=wp.time.year,
+                    month=wp.time.month,
+                    day=wp.time.day))
+        if popup:
+            pop_str = "<br>".join(popup)
+        else:
+            pop_str = "None"
+        return pop_str
+
     def __add_track_to_map(self, track, m):
         for segment in track.segments:
             points = []
@@ -121,7 +158,7 @@ class GPS():
                 points.append((point.latitude, point.longitude))
             folium.PolyLine(points).add_to(m)
 
-    def make_kml(self, output=None, waypoints=True, current_track=True, 
+    def make_kml(self, output=None, waypoints=True, current_track=True,
             archived_track=True, auto_open=True):
         kml = simplekml.Kml()
         if waypoints:
@@ -129,12 +166,12 @@ class GPS():
                 self.get_waypoints()
             for wp in self.waypoints:
                 kml.newpoint(
-                    name=wp.name, 
+                    name=wp.name,
                     coords=[(wp.longitude, wp.latitude, wp.elevation)],
-                    description=None)
+                    description=self.__wp_data_str(wp, name=False))
         if current_track:
             if not self.current_track:
-                self.get_current_tracks() 
+                self.get_current_tracks()
             for track in self.current_track:
                 self.__add_track_to_kml(track, kml)
         if archived_track:
@@ -145,7 +182,7 @@ class GPS():
         if output:
             kml_path = output
         else:
-            kml_path = os.path.join(os.getcwd(), 
+            kml_path = os.path.join(os.getcwd(),
                     "{}.kml".format(self.device_name))
         kml.save(kml_path)
         if auto_open:
@@ -159,5 +196,4 @@ class GPS():
             ls = kml.newlinestring(name=None, coords=coords)
             ls.style.linestyle.width = 2
             ls.style.linestyle.color = simplekml.Color.blue
-    
-   
+
